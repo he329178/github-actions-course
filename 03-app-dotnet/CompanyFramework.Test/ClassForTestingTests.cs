4fc34f8accbdade7897e6477e4298ea7f33a1f7f
he329178 <135258281+he329178@users.noreply.github.com>
2023-06-06 19:36:07 +0530

    Create ClassForTestingTests.cs

---
+++ 03-app-dotnet/CompanyFramework.Test/ClassForTestingTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace CompanyFramework.Test
+{
+    public class ClassForTestingTests
+    {
+        [Fact]
+        public void Test1()
+
+            // Assert
+            Assert.True(1==1);
+        }
+    }
+}
